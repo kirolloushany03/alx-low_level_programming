b85ffa828c9cf6ee2efc1c871fdfed2fc2f28c29
root <root@DESKTOP-7O5VQU7.localdomain>
2024-05-09 04:50:33 +0300

    new update

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,27 +1,44 @@
 #include "search_algos.h"
 
+/**
+ * printArray - Prints the elements of an array.
+ * @arr: Pointer to the first element of the array.
+ * @size: Size of the array.
+ *
+ * Return: void
+ */
 void printArray(int *arr, int size)
 {
 	int i;
+
 	printf("Searching in array: ");
 	for (i = 0; i < size; i++)
 	{
 		if (i < size - 1)
-		{ /*Check if the current element is not the last one*/
+		{ /* Check if the current element is not the last one */
 			printf("%d, ", arr[i]);
 		}
 		else
 		{
-			/*Print the last element without a comma*/
+			/* Print the last element without a comma */
 			printf("%d", arr[i]);
 		}
 	}
 	printf("\n");
 }
 
+/**
+ * binary_search - Searches for a value in an array.
+ * @array: Pointer to the first element of the array.
+ * @size: Size of the array.
+ * @value: Value to search for.
+ *
+ * Return: Index of the value if found, -1 otherwise.
+ */
 int binary_search(int *array, size_t size, int value)
 {
 	int low, high, mid;
+
 	low = 0;
 	high = size - 1;
 
@@ -34,10 +51,10 @@ int binary_search(int *array, size_t size, int value)
 
 	while (low <= high)
 	{
-		mid = (low + high) / 2;	 /*here we got the average*/
-		if (value == array[mid]) /*here i was putting it mid only without array*/
+		mid = (low + high) / 2;	 /* Here we got the average */
+		if (value == array[mid]) /* Here I was putting it mid only without array */
 		{
-			return mid;
+			return (mid);
 		}
 
 		if (value > array[mid])
@@ -48,7 +65,8 @@ int binary_search(int *array, size_t size, int value)
 		{
 			high = mid - 1;
 		}
-		/*first try i was putting this one without */
+
+		/* First try I was putting this one without */
 		if (high - low + 1 > 0)
 		{
 			printArray(array + low, high - low + 1);
