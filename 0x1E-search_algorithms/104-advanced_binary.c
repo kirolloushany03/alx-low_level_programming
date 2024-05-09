75473afac2bf0ead36794a1dab416546b45bc9ec
root <root@DESKTOP-7O5VQU7.localdomain>
2024-05-09 09:28:20 +0300

    new update

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -8,13 +8,14 @@ int advanced_binary_rec(int *array, size_t min, size_t max, int value);
  * @size: Number of elements in the array.
  * @value: Value to search for.
  *
- * Return: Index where value is located, or -1 if value is not present in array.
+ * Return: Index where value
+ * is located, or -1 if value is not present in array.
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-    if (!array)
-        return (-1);
-    return (advanced_binary_rec(array, 0, size - 1, value));
+	if (!array)
+		return (-1);
+	return (advanced_binary_rec(array, 0, size - 1, value));
 }
 
 /**
@@ -24,26 +25,27 @@ int advanced_binary(int *array, size_t size, int value)
  * @max: Maximum index of the current subarray.
  * @value: Value to search for.
  *
- * Return: Index where value is located, or -1 if value is not present in array.
+ * Return: Index where value
+ * is located, or -1 if value is not present in array.
  */
 int advanced_binary_rec(int *array, size_t min, size_t max, int value)
 {
-    size_t i = min;
-    size_t mid = min + (max - min) / 2;
-    int currentValue = array[mid];
+	size_t i = min;
+	size_t mid = min + (max - min) / 2;
+	int currentValue = array[mid];
 
-    if (min > max)
-        return (-1);
+	if (min > max)
+		return (-1);
 
-    printf("Searching in array: ");
-    while (i < max)
-        printf("%d, ", array[i++]);
-    printf("%d\n", array[i]);
+	printf("Searching in array: ");
+	while (i < max)
+		printf("%d, ", array[i++]);
+	printf("%d\n", array[i]);
 
-    if (currentValue == value && (mid == min || array[mid - 1] != value))
-        return (mid);
-    else if (currentValue >= value)
-        return (advanced_binary_rec(array, min, mid, value));
-    else
-        return (advanced_binary_rec(array, mid + 1, max, value));
+	if (currentValue == value && (mid == min || array[mid - 1] != value))
+		return (mid);
+	else if (currentValue >= value)
+		return (advanced_binary_rec(array, min, mid, value));
+	else
+		return (advanced_binary_rec(array, mid + 1, max, value));
 }
