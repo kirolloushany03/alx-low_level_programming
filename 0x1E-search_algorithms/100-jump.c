cedc8de5f9b9da23e9504d13d2da12f97e5729b7
root <root@DESKTOP-7O5VQU7.localdomain>
2024-05-09 05:35:27 +0300

    new update

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -7,7 +7,7 @@
  * @value: Value to search for.
  *
  * Return: The first index where value is located,
- * or -1 if value is not present in array or if array is NULL
+ * or -1 if value is not present in array or if array is NULL.
  */
 int jump_search(int *array, size_t size, int value)
 {
